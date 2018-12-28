12c44e3f70a38c4b99208e51eb1c3a51d235bad7
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-28 21:34:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,13 @@
 def second_challenge
-epic_tragedy = {
+epic_tragedy ={
    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-         :hero_friends => [
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    },
    :capulet => {
       :patriarch => {},
